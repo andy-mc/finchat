c110014128791d067b38ac74ef7562fe2f0e853b
Andrés Muñoz <asmz5488@gmail.com>
2017-07-10 19:32:51 -0500

    finbot 100% operational

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
-FinChat
-=======
+FinChat with Django
+========+++++++++++
 
 Simple chat with a bot integrated that makes queries to yahoo finance.
 
-Users has a profile page where all messages from a given user can be seen.
+Users has a profile page where all messages from the log-in user can be seen.
 
-The authentication, signup, loging and logout are built on top of djangos auth
-system.
+The authentication, signup, loging and logout are built on top of djangos auth-system.
 
 
 >> How to running locally:
 
-Create a virtual env for the project, the project runs in python 3
+Create a virtual env for the project (you'll need python 3)
 and them install the requirements with:
 
     pip install -r requirements.txt
 
-The project uses channel, for this reason you will need Redis 
-running locally as well, for install redis on mac you can use brew:
+The project uses channel, for this reason you'll need Redis running locally,
+for install redis on mac you can use brew:
 
     brew install redis
 
@@ -31,19 +30,28 @@ You can create a superuser acount for logging in and out with:
     
     python manage.py createsuperuser
 
-Finally migrate the proyects model and run the project locally with:
+Finally migrate the proyects models and run the project locally with:
 
+    python manage.py makemigrations
     python manage.py migrate
     python manage.py runserver
 
 
 >> How to use the finBot:
 
+    Simply send a chat message of the format /command=stock
+    for example, /stock=AAPL and the bot will return the financial information:
+        AAPL (Apple Inc.) quote is $144.18 per share
+
+    The bot has 2 commands:
+     1) /stock=AAPL  -->  return the close price for a given stock.
+     2) /day_range=AAPL  -->  return the day high/low price for a given stock.  
+
 
 FinChat Notes:
 ==============
 
-I take a lot of ideas of how develope the chat from this 2 tutorials:
+I take some ideas of how develope the chat from this 2 tutorials:
     
     >> https://gearheart.io/blog/creating-a-chat-with-django-channels
     >> https://blog.heroku.comin_deep_with_django_channels_the_future_of_real_time_apps_in_django
--- finbot/utils.py
+++ finbot/utils.py
@@ -1,37 +1,60 @@
+import bs4 as bs
+import urllib.request
+
 from datetime import datetime
 
 
 def stock_bot(stock):
     """
-    ????????
+    Make a query to yahoo to get the stock_name and the previous close price of a given stock
     """
-    return "APPL (apple INC) quote is $93.42 per share"
+    link = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22{}%22)&env=store://datatables.org/alltableswithkeys"
+    link = link.format(stock)
+    source = urllib.request.urlopen(link).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+
+    table = soup.find('quote')
+    stock_name = table.find('name').text
+    previous_close = table.find('previousclose').text
+
+    return "{} ({}) quote is ${} per share".format(stock, stock_name, previous_close)
 
 
 def day_range_stock_bot(stock):
     """
-    ????????
+    Make a query to yahoo to get the stock_name and day low/high prices of a given stock
     """
-    return "APPL (apple INC) Days Low quote is $ 92.11 and Days High quote is $94.15"
+    link = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22{}%22)&env=store://datatables.org/alltableswithkeys"
+    link = link.format(stock)
+    source = urllib.request.urlopen(link).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+
+    table = soup.find('quote')
+    stock_name = table.find('name').text
+    days_low = table.find('dayslow').text
+    days_high = table.find('dayshigh').text
+
+    return "{} ({}) Days Low quote is ${} and Days High quote is ${}".format(stock, stock_name, days_low, days_high)
 
 
-def finbot(command):
+def finbot(user_message):
     """
-    ????????
+    Take a user_message compounded by a command and a stock 
+    and return the bot_response to given command.
     """
-    action, stock = command.strip().split('=')
+    command, stock = user_message.strip().split('=')
     now = datetime.now().strftime('%b %-d %-I:%M %p')
 
-    if action.strip() == '/stock':
+    if command.strip() == '/stock':
         stock_quote = stock_bot(stock)
 
-    if action.strip() == '/day_range':
+    if command.strip() == '/day_range':
         stock_quote = day_range_stock_bot(stock)
 
-    bot_message = {
+    bot_response = {
         "username": "finbot",
         "message": stock_quote,
         "timestamp": now,
     }
 
-    return bot_message
+    return bot_response
