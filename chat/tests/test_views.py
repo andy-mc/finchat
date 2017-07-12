c060cb2fe43e7dd9a745584a3574b878d274d8f6
Andrés Muñoz <asmz5488@gmail.com>
2017-07-12 06:05:14 -0500

    Test UserDetail view

--- chat/tests/test_views.py
+++ chat/tests/test_views.py
@@ -1,6 +1,6 @@
 from chat.factories import MessageFactory, UserFactory
 from chat.models import Message
-from chat.views import home
+from chat.views import UserDetail, home
 from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
 
@@ -13,6 +13,15 @@ class TestHome(TestCase):
     def test_home_url(self):
         url = resolve('/')
         self.assertEqual(url.func, home)
+
+    def test_authentication_control(self):
+        '''Checks only authenticated users can view the page'''
+        response = self.client.get(reverse(home))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.login(username="user", password="test")
+        response = self.client.get(reverse(home))
+        self.assertEqual(response.status_code, 200)
 
     def test_home_template(self):
         self.client.login(username="user", password="test")
@@ -32,3 +41,50 @@ class TestHome(TestCase):
         self.assertEqual(len(db_messages), 60)
         self.assertEqual(list(home_messages),
                          list(db_messages.order_by('timestamp')[:50]))
+
+
+class TestUserDetail(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory(username="user")
+
+    def test_user_detail_url(self):
+        url = resolve('/users/{}/'.format(self.user.pk))
+        self.assertEqual(url.func.__name__, UserDetail.__name__)
+
+    def test_authentication_control(self):
+        '''Checks only authenticated users can view the page'''
+        response = self.client.get(reverse('user-detail',
+                                           kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.login(username="user", password="test")
+        response = self.client.get(reverse('user-detail',
+                                           kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_detail_template(self):
+        self.client.login(username="user", password="test")
+        response = self.client.get(reverse('user-detail',
+                                           kwargs={'pk': self.user.pk}))
+        self.assertTemplateUsed(response, 'chat/user-detail.html')
+
+    def test_user_detail_message_history(self):
+        # Create 2 message for test
+        MessageFactory.create_batch(2, user=self.user)
+        # Create 4 messages for random test users  
+        MessageFactory.create_batch(4)
+
+        self.client.login(username="user", password="test")
+        response = self.client.get(reverse('user-detail',
+                                           kwargs={'pk': self.user.pk}))
+
+        response_messages = response.context['messages']
+        db_messages = Message.objects.all()
+        user_messages = db_messages.filter(user=self.user
+                                  ).order_by('-timestamp')
+
+        self.assertEqual(len(db_messages), 6)
+        self.assertEqual(len(response_messages), 2)
+        self.assertEqual(list(response_messages),
+                         list(user_messages))
